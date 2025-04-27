1139f48bf80fde589e7eb2ca4170be701ad850cc
Mk00036 <mkv0369@gmail.com>
2025-04-27 17:19:32 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import NavBar from "./components/Layouts/NavBar/navBar"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+     <NavBar />
+    </BrowserRouter>
+  )
+}
+
+export default App
